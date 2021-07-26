c4df95d0b712f510666998eb35235e761dd378e3
hisbvdis <hisbvdis@gmail.com>
2021-07-26 17:23:00 +0300

    Fixes

--- nav/1static-under-logo/js/main.js
+++ nav/1static-under-logo/js/main.js
@@ -3,16 +3,16 @@ var navBtn = document.querySelector('.navBtn');
 
 
 // Для скрытия навигации в мобильной версии и определения срабатывания JS, у навигации:
-//  - удалить класс "openedNav" для скрытия контента в мобильной версии
+//  - удалить класс "navOpened" для скрытия контента в мобильной версии
 //  - удалить класс "no-js" для определения срабатывания JS
 function navOpenedRemove() {
   page.classList.remove('noJS');
-  page.classList.remove('openedNav');
+  page.classList.remove('navOpened');
 }
 document.onload = navOpenedRemove();
 
 
-// При нажатии на кнопку навигации, переключать класс ".openedNav"
+// При нажатии на кнопку навигации, переключать класс ".navOpened"
 navBtn.addEventListener('click', function(e) {
-  page.classList.toggle('openedNav');
+  page.classList.toggle('navOpened');
 })
